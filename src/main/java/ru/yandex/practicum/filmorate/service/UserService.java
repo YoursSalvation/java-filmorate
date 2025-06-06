ce70f5c9f21ce63beac34f71570df5ac083ad17e
Dmitry Prokhorov <prokhorovds26@mail.ru>
2025-06-06 16:51:55 +0500

    fourth try

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserStorage;
 
@@ -40,20 +41,20 @@ public class UserService {
     public void addFriend(Long id1, Long id2) {
         if (id1 == null || id1 < 1) throw new NotFoundException("Некорректный id");
         if (id2 == null || id2 < 1) throw new NotFoundException("Некорректный id");
-        if (Objects.equals(id1, id2)) throw new IllegalArgumentException("id должны различаться");
+        if (Objects.equals(id1, id2)) throw new ValidationException("id должны различаться");
         userStorage.addFriend(id1, id2);
     }
 
     public void removeFriend(Long id1, Long id2) {
         if (id1 == null || id1 < 1) throw new NotFoundException("Некорректный id");
         if (id2 == null || id2 < 1) throw new NotFoundException("Некорректный id");
-        if (Objects.equals(id1, id2)) throw new IllegalArgumentException("id должны различаться");
+        if (Objects.equals(id1, id2)) throw new ValidationException("id должны различаться");
         userStorage.removeFriend(id1, id2);
     }
 
     public Set<User> findMutualFriends(Long id1, Long id2) {
         if (id1 == null || id2 == null || id1 < 1 || id2 < 1) throw new IllegalArgumentException("Некорректный id");
-        if (Objects.equals(id1, id2)) throw new IllegalArgumentException("id должны различаться");
+        if (Objects.equals(id1, id2)) throw new ValidationException("id должны различаться");
         Set<User> mutualFriends = new HashSet<>();
         Set<Long> friendsUser = userStorage.getUserById(id1).getFriends();
         Set<Long> friendsOtherUser = userStorage.getUserById(id2).getFriends();
