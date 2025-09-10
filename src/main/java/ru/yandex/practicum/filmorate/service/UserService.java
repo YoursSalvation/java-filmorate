6dcb496ed91bb285f4bbd99aa46d48afe024f86e
Dmitry Prokhorov <prokhorovds26@mail.ru>
2025-09-10 16:37:47 +0500

    fix null birthday problem

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -50,8 +50,10 @@ public class UserService {
                 " электронный почты не может быть пустым и должен содержать символ '@'");
         if (user.getLogin().isBlank() || user.getLogin().contains(" ")) throw new ValidationException("Логин не может" +
                 " быть пустым и содержать пробелы");
-        if (user.getBirthday().isAfter(LocalDate.now()))
-            throw new ValidationException("Дата рождения не может быть в будущем");
+        if (user.getBirthday() == null) {
+            if (user.getBirthday().isAfter(LocalDate.now()))
+                throw new ValidationException("Дата рождения не может быть в будущем");
+        }
         User newUser = userStorage.createUser(user);
         return UserMapper.toDto(newUser);
     }
